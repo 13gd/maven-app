1ba8ee272996b3da74f99a1f917b53e6542ff5be
13gd <akashwaje99@gmail.com>
2026-01-06 16:51:11 +0000

    my mavenApp commit

---
+++ src/main/com/fortune/app/app.java
@@ -0,0 +1,32 @@
+package com.fortune.app;
+
+ 
+
+/**
+* welcome to m maven app 
+*/
+public class App {
+
+ 
+
+    private static final String MESSAGE = "Hello World!";
+
+ 
+
+    public App() {}
+
+ 
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+ 
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
+
+
+
