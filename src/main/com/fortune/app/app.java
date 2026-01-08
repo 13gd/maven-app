64808ae3e30ff4c38c5635f8c0e501064c324370
13gd <akashwaje99@gmail.com>
2026-01-08 05:26:42 +0000

    updated texxt

--- src/main/com/fortune/app/app.java
+++ src/main/com/fortune/app/app.java
@@ -1,32 +1,18 @@
-package com.fortune.app;
-
- 
-
 /**
-* this my maven application for testing
-*/
+ * This is my Maven application for testing
+ */
 public class App {
-
- 
 
     private static final String MESSAGE = "welcome to my application";
 
- 
-
     public App() {}
-
- 
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
     }
 
- 
-
     public String getMessage() {
         return MESSAGE;
     }
 }
-
-
 
